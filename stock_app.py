1eadce84bcfe1f9f7223e4ae017ddf3a96f1ac5e
B0743011 <125639519+B0743011@users.noreply.github.com>
2023-03-28 11:31:30 +0800

    Update stock_app.py

--- stock_app.py
+++ stock_app.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1aUPNrDIlWRVVrZO5x8zcX5LPdxxp6OPp
 """
 
-
+pip install streamlit
 
 import streamlit as st
 import pandas as pd
